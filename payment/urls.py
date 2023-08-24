9824aa6e65fc50055beea5fca636aa70d78c7ad8
Nikkeljohn <nikkeljohn120@gmail.com>
2023-08-24 14:30:23 +0000

    added stripe payment method and success templates

--- payment/urls.py
+++ payment/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.payment, name='payment')
+    path('', views.payment, name='payment'),
+    path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
 ]
