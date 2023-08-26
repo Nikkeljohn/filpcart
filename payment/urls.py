4c209422447ef525e0fb0aad6534dcef5e20a527
Nikkeljohn <nikkeljohn120@gmail.com>
2023-08-26 10:52:54 +0000

    added methodes and views to webhook.py and handeler

--- payment/urls.py
+++ payment/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.payment, name='payment'),
     path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
+    path('wh/', webhook, name='webhook'),
 ]
