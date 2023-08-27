8f13c087d85d63ff42e10b6215710fc8f13ac468
Nikkeljohn <nikkeljohn120@gmail.com>
2023-08-27 10:54:01 +0000

    added webhook and stripe functions

--- payment/urls.py
+++ payment/urls.py
@@ -5,6 +5,6 @@ from .webhooks import webhook
 urlpatterns = [
     path('', views.payment, name='payment'),
     path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
-    path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
+     path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
     path('wh/', webhook, name='webhook'),
 ]
