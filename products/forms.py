971e8295aae709e7e4fab69447ccf8bfb21c8b36
Nikkeljohn <nikkeljohn120@gmail.com>
2023-11-05 10:20:45 +0000

    fixed problem while adding product by site admin

--- products/forms.py
+++ products/forms.py
@@ -1,6 +1,5 @@
 from django import forms
-from .widgets import CustomClearableFileInput
-from .models import Product, Category, Review
+from .models import Product, Category
 
 
 class ProductForm(forms.ModelForm):
@@ -8,10 +7,6 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
-
-    image = forms.ImageField(label='Image',
-                             required=False,
-                             widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -21,10 +16,3 @@ class ProductForm(forms.ModelForm):
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
-
-
-class ReviewForm(forms.ModelForm):
-    class Meta:
-        model = Review
-        fields = ('body',)
-        
