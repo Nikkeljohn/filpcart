d43c7e0eb3453eb33a715a5166dc2bd2be52a018
Nikkeljohn <nikkeljohn120@gmail.com>
2023-11-05 09:52:30 +0000

    made changes in product app and path in templates, urls, views and models

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Review
 
-# Register your models here.
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
@@ -9,11 +8,11 @@ class ProductAdmin(admin.ModelAdmin):
         'name',
         'category',
         'price',
-        'rating',
         'image',
     )
 
     ordering = ('sku',)
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
@@ -21,5 +20,17 @@ class CategoryAdmin(admin.ModelAdmin):
         'name',
     )
 
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'product', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_reviews']
+
+    def approve_reviews(self, request, queryset):
+        queryset.update(approved=True)
