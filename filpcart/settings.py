7c95904fa6c1b4a5d288954b61970ab34bf0d11c
Nikkeljohn <nikkeljohn120@gmail.com>
2023-12-11 23:17:57 +0000

    Add 404 error functionality

--- filpcart/settings.py
+++ filpcart/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', '!oq_f2)al5)-7wz-pqil)%k#jlvj4wp3x*z2^3$bflu@6(3=d@')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'bag',
     'payment',
     'profiles',
+    
 
 
      # Other
--- filpcart/urls.py
+++ filpcart/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include 
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,3 +33,4 @@ urlpatterns = [
     
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+handler404 = 'filpcart.views.handler404'
