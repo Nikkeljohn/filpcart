b8d253bc634c848f15adac7325afda4656b73572
Nikkeljohn <nikkeljohn120@gmail.com>
2023-12-11 23:26:13 +0000

    Deleted env.py file

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-os.environ["SECRET_KEY"]="!oq_f2)al5)-7wz-pqil)%k#jlvj4wp3x*z2^3$bflu@6(3=d@"
-os.environ["STRIPE_PUBLIC_KEY"]
-os.environ["DEBUG"] = 'True'
-os.environ["DEVELOPMENT"] = 'True'
