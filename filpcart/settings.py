3d7fea5ba787ed4687d837354b618f42e53432b4
Nikkeljohn <nikkeljohn120@gmail.com>
2023-12-19 10:19:49 +0000

    deploying to heroku

--- filpcart/settings.py
+++ filpcart/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = ['https://git.heroku.com/myhome-1.git','localhost','8000-nikkeljohn-filpcart-6kust116qr1.ws-eu107.gitpod.io']
+ALLOWED_HOSTS = ['myhome-1-30d4043086e4.herokuapp.com','localhost','8000-nikkeljohn-filpcart-6kust116qr1.ws-eu107.gitpod.io']
 
 
 # Application definition
