c376993ad1938058a6119764b0a1afa071b73783
Nikkeljohn <nikkeljohn120@gmail.com>
2024-01-30 09:52:20 +0000

    change setting

--- filpcart/settings.py
+++ filpcart/settings.py
@@ -190,7 +190,6 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL = '/static/'
 
 if 'DEVELOPMENT' in os.environ:
     STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
