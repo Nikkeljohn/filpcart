222b499c3512aae37dbfb4a91e3928af3c063d31
Nikkeljohn <nikkeljohn120@gmail.com>
2024-04-24 21:30:40 +0000

    add blog image

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,7 @@ class Post(models.Model):
         User, on_delete=models.CASCADE, related_name='blog_post')
     updated_on = models.DateTimeField(auto_now=True)
     featured_image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
     content = models.TextField(null=True)
     price = models.DecimalField( max_digits=6, decimal_places=2,)
     excerpt = models.TextField(blank=True)
