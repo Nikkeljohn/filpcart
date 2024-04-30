2926fa39f7e14f1385f93fbd4bc9c3f461630623
Nikkeljohn <nikkeljohn120@gmail.com>
2024-04-30 19:29:51 +0000

    initital commit

--- home/views.py
+++ home/views.py
@@ -13,13 +13,13 @@ def index(request):
 
 def contact_us(request):
     context={}
-    if request.method=="POST":
+    if request.method == "POST":
         name = request.POST.get("name")
         em = request.POST.get("email")
         sub = request.POST.get("subject")
         msz = request.POST.get("message")
         
-        obj = Contact(name=name, email=em, subject=sub, message=msz)
+        obj = Contact(name = name, email = em, subject = sub, message = msz)
         obj.save()
         context['message']=f"Dear {name}, Thanks for your time!"
 
